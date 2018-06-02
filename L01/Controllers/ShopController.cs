9eacb6b765147d5533b448a71c5017989150ad3d
greif.matthias <greif.matthias@outlook.com>
2018-06-02 22:07:21 +0200

    Part 04
    Endcode of MotoGP Part 04

--- L01/Controllers/ShopController.cs
+++ L01/Controllers/ShopController.cs
@@ -2,23 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using L01.Data;
+using L01.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace L01.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly GPContext context;
+
+        public ShopController(GPContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult OrderTicket()
         {
             ViewData["BannerNr"] = 3;
 
+            ViewData["Countries"] = new SelectList(context.Countries.OrderBy(c => c.Name), "CountryId", "Name");
+            ViewData["Races"] = context.Races.OrderBy(c => c.Name).ToList<Race>();
+
             return View();
         }
 
-        public IActionResult ConfirmOrder()
+        public IActionResult ConfirmOrder(int id)
         {
             ViewData["BannerNr"] = 3;
 
+            Ticket ticket = context.Tickets.Where(t => t.TicketId == id).Single();
+            ticket.Race = context.Races.Where(r => r.RaceId == ticket.RaceId).Single();
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create([Bind("TicketId, Name, Email, Address, Number, CountryId, RaceId")] Ticket ticket)
+        {
+            ticket.Paid = false;
+            ticket.OrderDate = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                context.Add(ticket);
+                context.SaveChanges();
+
+                return RedirectToAction("ConfirmOrder", new { id = ticket.TicketId });
+            }
+            
             return View();
         }
     }
